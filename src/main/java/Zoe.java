666a3b3b7ce17979adea471517439b5273645e2c
marcusxu <marcus.xu@u.nus.edu>
2025-01-21 23:50:35 +0800

    Level-0

---
+++ src/main/java/Zoe.java
@@ -0,0 +1,6 @@
+public class Zoe {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Zoe\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
