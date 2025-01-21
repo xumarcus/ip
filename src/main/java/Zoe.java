69dab19ada2cf65d00ed4a36e2bcd04ade7c1379
marcusxu <marcus.xu@u.nus.edu>
2025-01-22 00:02:43 +0800

    Level-2

--- src/main/java/Zoe.java
+++ src/main/java/Zoe.java
@@ -1,13 +1,29 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Zoe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Zoe\nWhat can I do for you?\n");
+
+        ArrayList<String> tasks = new ArrayList<>();
+        io:
         while (true) {
             String command = scanner.nextLine();
-            if (command.equals("bye")) break;
-            System.out.println(command);
+            switch (command) {
+                case "bye":
+                    break io;
+                case "list":
+                    for (ListIterator<String> iter = tasks.listIterator(); iter.hasNext();) {
+                        System.out.printf("%d. %s\n", iter.nextIndex() + 1, iter.next());
+                    }
+                    break;
+                default:
+                    tasks.add(command);
+                    System.out.printf("added: %s\n", command);
+                    break;
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
