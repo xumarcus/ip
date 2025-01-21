f9603b836a3f82abb0432b75fa90e0d907c17102
marcusxu <marcus.xu@u.nus.edu>
2025-01-22 02:25:30 +0800

    Level-5

--- src/main/java/TodoExecutor.java
+++ src/main/java/TodoExecutor.java
@@ -2,16 +2,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TodoExecutor implements Executor {
-    private static final Pattern PATTERN = Pattern.compile("^todo (.+)$");
+    private static final Pattern PATTERN = Pattern.compile("^todo(.+)$");
 
     public TodoExecutor() {
     }
 
     @Override
-    public boolean run(String command, TaskList taskList) {
+    public boolean run(String command, TaskList taskList) throws ZoeException {
         Matcher matcher = PATTERN.matcher(command);
         if (matcher.matches()) {
-            String taskName = matcher.group(1);
+            String taskName = matcher.group(1).trim();
+            if (taskName.isEmpty()) {
+                throw new ZoeException("OOPS!!! The description of a todo cannot be empty.");
+            }
             Task task = new ToDo(taskName);
             taskList.add(task);
             return true;
---
+++ src/main/java/ZoeException.java
@@ -0,0 +1,5 @@
+public class ZoeException extends Exception {
+    public ZoeException(String message) {
+        super(message);
+    }
+}
