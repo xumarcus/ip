abd53870a97277dc05ba9017137228c8da36eb87
marcusxu <marcus.xu@u.nus.edu>
2025-01-21 23:53:11 +0800

    Level-1

--- src/main/java/Zoe.java
+++ src/main/java/Zoe.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Zoe {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Zoe\nWhat can I do for you?\n");
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) break;
+            System.out.println(command);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
