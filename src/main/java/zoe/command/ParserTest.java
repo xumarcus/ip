a8516fa583b0b6d33f2c91681afcc2e13b459cd5
marcusxu <marcus.xu@u.nus.edu>
2025-01-26 22:38:00 +0800

    Add test for Parser

---
+++ src/main/java/zoe/command/ParserTest.java
@@ -0,0 +1,28 @@
+package zoe.command;
+
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTest {
+    @Test
+    public void testParseTodoCommand() {
+        Command command = Parser.parse("todo Read book");
+        assertInstanceOf(TodoCommand.class, command);
+        assertEquals("Read book", ((TodoCommand) command).getTaskName());
+    }
+
+    @Test
+    public void testParseDeadlineCommand() {
+        Command command = Parser.parse("deadline Submit assignment /by 2024-12-01");
+        assertInstanceOf(DeadlineCommand.class, command);
+        assertEquals("Submit assignment", ((DeadlineCommand) command).getTaskName());
+        assertEquals(LocalDate.of(2024, 12, 1), ((DeadlineCommand) command).getBy());
+    }
+
+    @Test
+    public void testParseIncorrectCommand() {
+        Command command = Parser.parse("");
+        assertInstanceOf(IncorrectCommand.class, command);
+    }
+}
