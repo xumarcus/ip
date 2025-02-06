ba70d8e6eec4b6e0106cac3aa8351f2416cdd0c9
marcusxu <marcus.xu@u.nus.edu>
2025-02-06 18:16:41 +0800

    Remove unused import

--- src/main/java/zoe/command/Parser.java
+++ src/main/java/zoe/command/Parser.java
@@ -5,8 +5,6 @@ import java.time.format.DateTimeParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import zoe.task.Todo;
-
 /**
  * The {@code Parser} class processes user inputs and converts them into executable {@code Command} objects.
  * It supports various command formats for a task management application.
